26123a75af7ff6f68aa8e1b6af67aefc29d6565f
eolivelli <eolivelli@gmail.com>
2015-09-14 14:41:45 +0200

    MAJ-19

--- majordodo-core/src/main/java/majordodo/task/Broker.java
+++ majordodo-core/src/main/java/majordodo/task/Broker.java
@@ -75,7 +75,7 @@ public class Broker implements AutoCloseable, JVMBrokerSupportInterface, BrokerF
     }
 
     public static String VERSION() {
-        return "0.1.12-BETA1";
+        return "0.1.12-BETA2";
     }
 
     public static byte[] formatHostdata(String host, int port, Map<String, String> additional) {
