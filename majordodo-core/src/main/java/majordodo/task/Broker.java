81a84328cb0c2327b6c6fc2d2456bcb887ac9b83
eolivelli <eolivelli@gmail.com>
2015-10-08 15:49:09 +0200

    0.1.12 release

--- majordodo-core/src/main/java/majordodo/task/Broker.java
+++ majordodo-core/src/main/java/majordodo/task/Broker.java
@@ -102,7 +102,7 @@ public class Broker implements AutoCloseable, JVMBrokerSupportInterface, BrokerF
     }
 
     public static String VERSION() {
-        return "0.1.12-BETA7";
+        return "0.1.12";
     }
 
     private final Workers workers;
