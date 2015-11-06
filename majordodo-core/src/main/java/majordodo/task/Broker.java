37b4f50edeb95d77ae168c75f1231411be4e5433
eolivelli <eolivelli@gmail.com>
2015-11-06 14:50:44 +0100

    moving master to 0.1.19

--- majordodo-core/src/main/java/majordodo/task/Broker.java
+++ majordodo-core/src/main/java/majordodo/task/Broker.java
@@ -102,7 +102,7 @@ public class Broker implements AutoCloseable, JVMBrokerSupportInterface, BrokerF
     }
 
     public static String VERSION() {
-        return "0.1.18";
+        return "0.1.19";
     }
 
     private final Workers workers;
