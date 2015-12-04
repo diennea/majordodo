d85e9af7ce0bc587a68e3ae8f328def1eaf4759c
eolivelli <eolivelli@gmail.com>
2015-12-04 11:24:30 +0100

    MAJ-54

---
+++ majordodo-core/src/test/java/majordodo/task/SlotsReleaseTest.java
@@ -0,0 +1,349 @@
+/*
+ Licensed to Diennea S.r.l. under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. Diennea S.r.l. licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+
+ */
+package majordodo.task;
+
+import majordodo.task.BrokerConfiguration;
+import majordodo.task.TasksHeap;
+import majordodo.task.FileCommitLog;
+import majordodo.task.Task;
+import majordodo.task.GroupMapperFunction;
+import majordodo.task.Broker;
+import majordodo.clientfacade.SubmitTaskResult;
+import majordodo.clientfacade.TaskStatusView;
+import majordodo.executors.TaskExecutor;
+import majordodo.network.netty.NettyBrokerLocator;
+import majordodo.network.netty.NettyChannelAcceptor;
+import majordodo.worker.WorkerCore;
+import majordodo.worker.WorkerCoreConfiguration;
+import majordodo.worker.WorkerStatusListener;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.SimpleFormatter;
+import majordodo.clientfacade.AddTaskRequest;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Basic tests for recovery
+ *
+ * @author enrico.olivelli
+ */
+public class SlotsReleaseTest {
+
+    protected Path workDir;
+
+    @After
+    public void deleteWorkdir() throws Exception {
+        if (workDir != null) {
+            Files.walkFileTree(workDir, new FileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+
+            });
+        }
+
+    }
+
+    @Before
+    public void setupLogger() throws Exception {
+        Level level = Level.INFO;
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.err.println("uncaughtException from thread " + t.getName() + ": " + e);
+                e.printStackTrace();
+            }
+        });
+        java.util.logging.LogManager.getLogManager().reset();
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setLevel(level);
+        SimpleFormatter f = new SimpleFormatter();
+        ch.setFormatter(f);
+        java.util.logging.Logger.getLogger("").setLevel(level);
+        java.util.logging.Logger.getLogger("").addHandler(ch);
+    }
+
+    protected GroupMapperFunction createGroupMapperFunction() {
+        return new GroupMapperFunction() {
+
+            @Override
+            public int getGroup(long taskid, String tasktype, String userid) {
+                return groupsMap.getOrDefault(userid, 0);
+
+            }
+        };
+    }
+
+    protected Map<String, Integer> groupsMap = new HashMap<>();
+
+    private static final String TASKTYPE_MYTYPE = "mytype";
+    private static final String userId = "queue1";
+    private static final int group = 12345;
+
+    @Before
+    public void before() throws Exception {
+        groupsMap.clear();
+        groupsMap.put(userId, group);
+    }
+
+    @Test
+    public void slotReleaseOnErrorTest() throws Exception {
+
+        Path mavenTargetDir = Paths.get("target").toAbsolutePath();
+        workDir = Files.createTempDirectory(mavenTargetDir, "test" + System.nanoTime());
+        System.out.println("SETUPWORKDIR:" + workDir);
+        long taskId;
+        String workerId = "abc";
+        String taskParams = "param";
+        final String SLOTID = "myslot";
+
+        // startAsWritable a broker and request a task, with slot
+        try (Broker broker = new Broker(new BrokerConfiguration(), new MemoryCommitLog(), new TasksHeap(1000, createGroupMapperFunction()));) {
+            broker.startAsWritable();
+            SubmitTaskResult res = broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, SLOTID, 0));
+            taskId = res.getTaskId();
+            assertTrue(taskId > 0);
+            assertTrue(res.getOutcome() == null);
+            // slot is busy
+            assertEquals(0, broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, SLOTID, 0)).getTaskId());
+
+            try (NettyChannelAcceptor server = new NettyChannelAcceptor(broker.getAcceptor());) {
+                server.start();
+                try (NettyBrokerLocator locator = new NettyBrokerLocator(server.getHost(), server.getPort(), server.isSsl())) {
+
+                    CountDownLatch connectedLatch = new CountDownLatch(1);
+                    CountDownLatch disconnectedLatch = new CountDownLatch(1);
+                    CountDownLatch allTaskExecuted = new CountDownLatch(1);
+                    WorkerStatusListener listener = new WorkerStatusListener() {
+
+                        @Override
+                        public void connectionEvent(String event, WorkerCore core) {
+                            if (event.equals(WorkerStatusListener.EVENT_CONNECTED)) {
+                                connectedLatch.countDown();
+                            }
+                            if (event.equals(WorkerStatusListener.EVENT_DISCONNECTED)) {
+                                disconnectedLatch.countDown();
+                            }
+                        }
+
+                    };
+                    Map<String, Integer> tags = new HashMap<>();
+                    tags.put(TASKTYPE_MYTYPE, 1);
+
+                    WorkerCoreConfiguration config = new WorkerCoreConfiguration();
+                    config.setMaxPendingFinishedTaskNotifications(1);
+                    config.setWorkerId(workerId);
+                    config.setMaxThreadsByTaskType(tags);
+                    config.setGroups(Arrays.asList(group));
+                    try (WorkerCore core = new WorkerCore(config, workerId, locator, listener);) {
+                        core.start();
+                        assertTrue(connectedLatch.await(10, TimeUnit.SECONDS));
+                        core.setExecutorFactory(
+                                (String tasktype, Map<String, Object> parameters) -> new TaskExecutor() {
+
+                            @Override
+                            public String executeTask(Map<String, Object> parameters) throws Exception {
+                                allTaskExecuted.countDown();
+                                throw new Exception("error !");
+                            }
+
+                        }
+                        );
+
+                        assertTrue(allTaskExecuted.await(30, TimeUnit.SECONDS));
+
+                        boolean okFinishedForBroker = false;
+                        for (int i = 0; i < 100; i++) {
+                            TaskStatusView task = broker.getClient().getTask(taskId);
+                            if (task.getStatus() == Task.STATUS_ERROR) {
+                                okFinishedForBroker = true;
+                                break;
+                            }
+                            Thread.sleep(1000);
+                        }
+                        assertTrue(okFinishedForBroker);
+                    }
+                    assertTrue(disconnectedLatch.await(10, TimeUnit.SECONDS));
+
+                    // now the slot is free
+                    taskId = broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 0, 0, SLOTID, 0)).getTaskId();
+                    assertTrue(taskId > 0);
+                }
+
+            }
+        }
+
+    }
+
+    @Test
+    public void slotReleaseOnWorkerDeathTest() throws Exception {
+
+        Path mavenTargetDir = Paths.get("target").toAbsolutePath();
+        workDir = Files.createTempDirectory(mavenTargetDir, "test" + System.nanoTime());
+        System.out.println("SETUPWORKDIR:" + workDir);
+        long taskId;
+        String workerId = "abc";
+        String taskParams = "param";
+        final String SLOTID = "myslot";
+
+        // startAsWritable a broker and request a task, with slot
+        BrokerConfiguration bc = new BrokerConfiguration();
+        bc.setMaxWorkerIdleTime(1000);
+        try (Broker broker = new Broker(bc, new MemoryCommitLog(), new TasksHeap(1000, createGroupMapperFunction()));) {
+            broker.startAsWritable();
+            SubmitTaskResult res = broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, SLOTID, 0));
+            taskId = res.getTaskId();
+            assertTrue(taskId > 0);
+            assertTrue(res.getOutcome() == null);
+            // slot is busy
+            assertEquals(0, broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, SLOTID, 0)).getTaskId());
+
+            try (NettyChannelAcceptor server = new NettyChannelAcceptor(broker.getAcceptor());) {
+                server.start();
+                try (NettyBrokerLocator locator = new NettyBrokerLocator(server.getHost(), server.getPort(), server.isSsl())) {
+
+                    CountDownLatch connectedLatch = new CountDownLatch(1);
+                    CountDownLatch disconnectedLatch = new CountDownLatch(1);
+                    CountDownLatch allTaskExecuted = new CountDownLatch(1);
+                    WorkerStatusListener listener = new WorkerStatusListener() {
+
+                        @Override
+                        public void connectionEvent(String event, WorkerCore core) {
+                            if (event.equals(WorkerStatusListener.EVENT_CONNECTED)) {
+                                connectedLatch.countDown();
+                            }
+                            if (event.equals(WorkerStatusListener.EVENT_DISCONNECTED)) {
+                                disconnectedLatch.countDown();
+                            }
+                        }
+
+                    };
+                    Map<String, Integer> tags = new HashMap<>();
+                    tags.put(TASKTYPE_MYTYPE, 1);
+
+                    WorkerCoreConfiguration config = new WorkerCoreConfiguration();
+                    config.setMaxPendingFinishedTaskNotifications(1);
+                    config.setWorkerId(workerId);
+                    config.setMaxThreadsByTaskType(tags);
+                    config.setGroups(Arrays.asList(group));
+                    try (WorkerCore core = new WorkerCore(config, workerId, locator, listener);) {
+                        core.start();
+                        assertTrue(connectedLatch.await(10, TimeUnit.SECONDS));
+                        core.setExecutorFactory(
+                                (String tasktype, Map<String, Object> parameters) -> new TaskExecutor() {
+
+                            @Override
+                            public String executeTask(Map<String, Object> parameters) throws Exception {
+                                allTaskExecuted.countDown();
+                                core.die();
+                                throw new Exception("error");
+                            }
+                        }
+                        );
+                        assertTrue(allTaskExecuted.await(30, TimeUnit.SECONDS));
+                    }
+                    assertTrue(disconnectedLatch.await(10, TimeUnit.SECONDS));
+                }
+                boolean okFinishedForBroker = false;
+                for (int i = 0; i < 100; i++) {
+                    TaskStatusView task = broker.getClient().getTask(taskId);
+                    if (task.getStatus() == Task.STATUS_ERROR) {
+                        System.out.println("result: "+task.getResult());
+                        okFinishedForBroker = true;
+                        assertEquals("worker abc died",task.getResult());
+                        break;
+                    }
+                    Thread.sleep(1000);
+                }
+                assertTrue(okFinishedForBroker);
+                // now the slot is free
+                taskId = broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 0, 0, SLOTID, 0)).getTaskId();
+                assertTrue(taskId > 0);
+
+            }
+        }
+
+    }
+
+    @Test
+    public void slotReleaseOnDeadlineTest() throws Exception {
+
+        Path mavenTargetDir = Paths.get("target").toAbsolutePath();
+        workDir = Files.createTempDirectory(mavenTargetDir, "test" + System.nanoTime());
+        System.out.println("SETUPWORKDIR:" + workDir);
+        long taskId;
+        String workerId = "abc";
+        String taskParams = "param";
+        final String SLOTID = "myslot";
+
+        // startAsWritable a broker and request a task, with slot
+        try (Broker broker = new Broker(new BrokerConfiguration(), new MemoryCommitLog(), new TasksHeap(1000, createGroupMapperFunction()));) {
+            broker.startAsWritable();
+            SubmitTaskResult res = broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 0, System.currentTimeMillis(), SLOTID, 0));
+            taskId = res.getTaskId();
+            assertTrue(taskId > 0);
+            assertTrue(res.getOutcome() == null);
+            // slot is busy
+            assertEquals(0, broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 0, 0, SLOTID, 0)).getTaskId());
+            broker.purgeTasks();
+            // slot is free
+            long taskId2 = broker.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 0, 0, SLOTID, 0)).getTaskId();
+            assertTrue(taskId2 > 0);
+        }
+
+    }
+
+}
