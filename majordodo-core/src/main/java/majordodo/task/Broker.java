38d5db296f31a6fc5592970d81c9e08236b0d534
eolivelli <eolivelli@gmail.com>
2016-05-20 17:21:05 +0200

    Upgrade to version 0.4.0

--- majordodo-core/src/main/java/majordodo/task/Broker.java
+++ majordodo-core/src/main/java/majordodo/task/Broker.java
@@ -102,7 +102,7 @@ public final class Broker implements AutoCloseable, JVMBrokerSupportInterface, B
     }
 
     public static String VERSION() {
-        return "0.3.2";
+        return "0.4.0";
     }
 
     private final Workers workers;
