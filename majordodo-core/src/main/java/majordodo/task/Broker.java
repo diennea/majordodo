02701fe3505416349ae54f0c6e6bf199547fe5db
eolivelli <eolivelli@gmail.com>
2017-01-09 13:12:29 +0100

    0.8.0-ALPHA3

--- majordodo-core/src/main/java/majordodo/task/Broker.java
+++ majordodo-core/src/main/java/majordodo/task/Broker.java
@@ -112,7 +112,7 @@ public final class Broker implements AutoCloseable, JVMBrokerSupportInterface, B
     }
 
     public static String VERSION() {
-        return "0.8.0-ALPHA2";
+        return "0.8.0-ALPHA3";
     }
 
     private final Workers workers;
