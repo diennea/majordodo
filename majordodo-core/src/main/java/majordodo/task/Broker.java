8d4e9825e50ea527e1b69da5da7fd637866b4546
Francesco Caliumi - Diennea <francesco.caliumi@diennea.com>
2017-06-19 10:43:35 +0200

    0.11.0

--- majordodo-core/src/main/java/majordodo/task/Broker.java
+++ majordodo-core/src/main/java/majordodo/task/Broker.java
@@ -117,7 +117,7 @@ public final class Broker implements AutoCloseable, JVMBrokerSupportInterface, B
     }
 
     public static String VERSION() {
-        return "0.11.0-ALPHA2";
+        return "0.11.0";
     }
 
     private final Workers workers;
