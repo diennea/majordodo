93c259ff9048250073418260c3ca75b1dfa2f756
eolivelli <eolivelli@gmail.com>
2017-11-06 11:44:45 +0100

    bump version to  0.12.0-SNAPSHOT

--- majordodo-core/src/main/java/majordodo/task/Broker.java
+++ majordodo-core/src/main/java/majordodo/task/Broker.java
@@ -117,7 +117,7 @@ public final class Broker implements AutoCloseable, JVMBrokerSupportInterface, B
     }
 
     public static String VERSION() {
-        return "0.11.0";
+        return "0.12.0-SNAPSHOT";
     }
 
     private final Workers workers;
