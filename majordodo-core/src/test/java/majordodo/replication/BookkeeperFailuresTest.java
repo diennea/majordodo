fc3abc86022f00ac1c26b3f55e19c98315dffbf2
eolivelli <eolivelli@gmail.com>
2017-12-05 15:54:53 +0100

    use typed checks on BK Exception in testcases

--- majordodo-core/src/test/java/majordodo/replication/BookkeeperFailuresTest.java
+++ majordodo-core/src/test/java/majordodo/replication/BookkeeperFailuresTest.java
@@ -26,6 +26,8 @@ import majordodo.clientfacade.AddTaskRequest;
 import majordodo.network.BrokerHostData;
 import majordodo.task.BrokerTestUtils;
 import majordodo.task.LogNotAvailableException;
+import org.apache.bookkeeper.client.BKException.BKLedgerFencedException;
+import org.apache.bookkeeper.client.BKException.BKNotEnoughBookiesException;
 import org.apache.bookkeeper.client.BookKeeper;
 import org.apache.bookkeeper.client.LedgerHandle;
 import org.apache.bookkeeper.conf.ClientConfiguration;
@@ -41,33 +43,34 @@ import org.junit.Test;
  * @author francesco.caliumi
  */
 @BrokerTestUtils.StartReplicatedBrokers
-@BrokerTestUtils.LogLevel(level="SEVERE")
+@BrokerTestUtils.LogLevel(level = "SEVERE")
 public class BookkeeperFailuresTest extends BrokerTestUtils {
-        
+
     private final LeaderShipChangeListener leaderShiplistener = new LeaderShipChangeListener() {
         @Override
         public void leadershipLost() {
             System.out.println("leadershipLost");
         }
+
         @Override
         public void leadershipAcquired() {
             System.out.println("leadershipAcquired");
         }
     };
-    
+
     private BookKeeper createBookKeeper() throws Exception {
         byte[] localhostdata = BrokerHostData.formatHostdata(new BrokerHostData(broker1Host, broker1Port, "", false, null));
         ClientConfiguration config = new ClientConfiguration();
         config.setThrottleValue(0);
-        
+
         ZKClusterManager zkClusterManager = new ZKClusterManager(zkServer.getAddress(), zkServer.getTimeout(), zkServer.getPath(), leaderShiplistener, localhostdata, false);
         zkClusterManager.waitForConnection();
         BookKeeper bookKeeper = new BookKeeper(config, zkClusterManager.getZooKeeper());
         zkClusterManager.start();
-        
+
         return bookKeeper;
     }
-    
+
     @Before
     public void before() {
         broker1Config.setMaxWorkerIdleTime(1000);
@@ -82,44 +85,44 @@ public class BookkeeperFailuresTest extends BrokerTestUtils {
         tags.put(TASKTYPE_MYTYPE, 1);
 
         assertTrue(broker1.isWritable());
-        
+
         long taskId1 = broker1.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
         long taskId2 = broker1.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
-        
+
         try {
             long taskId3 = broker2.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
             fail();
         } catch (Exception ex) {
             assertEquals("java.lang.Exception: broker_not_leader", ex.getMessage());
         }
-        
+
         // Suspend flush in order to prevent the noop operation to alter the broker status before our test
         broker1.setSuspendLogFlush(true);
-        
+
         ReplicatedCommitLog broker1scl = (ReplicatedCommitLog) broker1.getStatusChangesLog();
-        
+
         try (BookKeeper bk = createBookKeeper();) {
             try (LedgerHandle fenceLedger = bk.openLedger(
                 broker1scl.getCurrentLedgerId(), BookKeeper.DigestType.MAC, "dodo".getBytes(StandardCharsets.UTF_8));) {
-            
+
                 try {
                     long taskId4 = broker1.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
                     fail();
                 } catch (LogNotAvailableException ex) {
-                    assertEquals("org.apache.bookkeeper.client.BKException$BKLedgerFencedException", ex.getMessage());
+                    assertTrue(ex.getCause() instanceof BKLedgerFencedException);
                 }
-                
+
                 assertFalse(broker1.isWritable());
                 assertFalse(broker2.isWritable());
             }
         }
-        
-        waitForBrokerToBecomeLeader(broker2, 30*1000);
-        
+
+        waitForBrokerToBecomeLeader(broker2, 30 * 1000);
+
         long taskId5 = broker2.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
         assertTrue(taskId5 > 0);
     }
-    
+
     @Test
     public void brokerUnavailableTest() throws Exception {
         String taskParams = "param";
@@ -128,27 +131,27 @@ public class BookkeeperFailuresTest extends BrokerTestUtils {
         tags.put(TASKTYPE_MYTYPE, 1);
 
         assertTrue(broker1.isWritable());
-        
+
         long taskId1 = broker1.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
         long taskId2 = broker1.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
-        
+
         try {
             long taskId3 = broker2.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
             fail();
         } catch (Exception ex) {
             assertEquals("java.lang.Exception: broker_not_leader", ex.getMessage());
         }
-        
+
         // Suspend flush in order to prevent the noop operation to alter the broker status before our test
         broker1.setSuspendLogFlush(true);
-        
+
         zkServer.stopBookie();
-        
+
         try {
             long taskId4 = broker1.getClient().submitTask(new AddTaskRequest(0, TASKTYPE_MYTYPE, userId, taskParams, 1, 0, 0, null, 0, null, null)).getTaskId();
             fail();
         } catch (LogNotAvailableException ex) {
-            assertEquals("org.apache.bookkeeper.client.BKException$BKNotEnoughBookiesException", ex.getMessage());
+            assertTrue(ex.getCause() instanceof BKNotEnoughBookiesException);
         }
     }
 }
