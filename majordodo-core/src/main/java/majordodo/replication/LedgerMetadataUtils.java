6f0e6885351a47e649fbfd4d0f13ac812c67afad
dmercuriali <48724410+dmercuriali@users.noreply.github.com>
2019-03-20 18:53:28 +0100

    Add BookKeeper Metadata, like Pulsar (#138) (#141)
    * Add BookKeeper Metadata, like Pulsar (#138)
    
    * Add BookKeeper Metadata, like Pulsar #138
    
    * Add BookKeeper Metadata, like Pulsar #138

---
+++ majordodo-core/src/main/java/majordodo/replication/LedgerMetadataUtils.java
@@ -0,0 +1,49 @@
+/*
+ Licensed to Diennea S.r.l. under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. Diennea S.r.l. licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+
+ */
+package majordodo.replication;
+
+import com.google.common.collect.ImmutableMap;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+/**
+ *
+ * @author dennis.mercuriali
+ */
+public class LedgerMetadataUtils {
+
+    private static final String APPLICATION_METADATA = "application";
+    private static final byte[] APPLICATION_METADATA_VALUE = "majordodo".getBytes(StandardCharsets.UTF_8);
+    
+    private static final String COMPONENT_METADATA = "component";
+    private static final byte[] COMPONENT_METADATA_VALUE = "broker".getBytes(StandardCharsets.UTF_8);
+    
+    private static final String CREATED_BY_METADATA = "broker-id";
+
+    public static Map<String, byte[]> buildBrokerLedgerMetadata(String brokerId) {
+        String createdByValue = brokerId == null ? "" : brokerId;
+        
+        return ImmutableMap.of(
+            APPLICATION_METADATA, APPLICATION_METADATA_VALUE,
+            COMPONENT_METADATA, COMPONENT_METADATA_VALUE,
+            CREATED_BY_METADATA, createdByValue.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+}
--- majordodo-core/src/test/java/majordodo/replication/ReplicatedCommitLogSimpleTest.java
+++ majordodo-core/src/test/java/majordodo/replication/ReplicatedCommitLogSimpleTest.java
@@ -19,15 +19,19 @@
  */
 package majordodo.replication;
 
+import java.nio.charset.StandardCharsets;
 import majordodo.task.BrokerStatusSnapshot;
 import majordodo.task.LogSequenceNumber;
 import majordodo.task.StatusEdit;
 import majordodo.task.Task;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import org.apache.bookkeeper.client.BookKeeper;
+import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -41,6 +45,8 @@ import org.junit.rules.TemporaryFolder;
  * @author enrico.olivelli
  */
 public class ReplicatedCommitLogSimpleTest {
+
+    private static final String BROKER_ID = "testBrokerId";
 
     @Rule
     public TemporaryFolder folderSnapshots = new TemporaryFolder();
@@ -70,6 +76,15 @@ public class ReplicatedCommitLogSimpleTest {
                 LogSequenceNumber logStatusEdit2 = log.logStatusEdit(edit2);
                 LogSequenceNumber logStatusEdit3 = log.logStatusEdit(edit3);
                 LogSequenceNumber logStatusEdit4 = log.logStatusEdit(edit4);
+
+                Map<String, byte[]> customMeta = log
+                    .getBookKeeper()
+                    .openLedgerNoRecovery(logStatusEdit4.ledgerId, BookKeeper.DigestType.MAC, log.getSharedSecret().getBytes(StandardCharsets.UTF_8))
+                    .getCustomMetadata();
+
+                Assert.assertEquals("majordodo", new String(customMeta.get("application"), StandardCharsets.UTF_8));
+                Assert.assertEquals("broker", new String(customMeta.get("component"), StandardCharsets.UTF_8));
+                Assert.assertEquals("", new String(customMeta.get("broker-id"), StandardCharsets.UTF_8));
             }
 
             try (ReplicatedCommitLog log = new ReplicatedCommitLog(zkServer.getAddress(), 40000, "/dodo", folderSnapshots.getRoot().toPath(), null, false);) {
@@ -95,7 +110,29 @@ public class ReplicatedCommitLogSimpleTest {
                 assertEquals(StatusEdit.TYPE_ASSIGN_TASK_TO_WORKER, edits.get(2).editType);
                 assertEquals("db1,db2", edits.get(2).resources);
                 assertEquals(StatusEdit.TYPE_TASK_STATUS_CHANGE, edits.get(3).editType);
+            }
 
+            try (ReplicatedCommitLog log = new ReplicatedCommitLog(zkServer.getAddress(), 40000, "/dodo",
+                folderSnapshots.getRoot().toPath(), null, false, Collections.emptyMap(), BROKER_ID);) {
+
+                log.getClusterManager().start();
+                log.requestLeadership();
+                BrokerStatusSnapshot snapshot = log.loadBrokerStatusSnapshot();
+                log.recovery(snapshot.getActualLogSequenceNumber(), (a, b) -> {
+                }, false);
+                log.startWriting();
+
+                StatusEdit edit1 = StatusEdit.ADD_TASK(1, "mytask", "param1", "myuser", 0, 0, 0, null, 0, null, null);
+                long ledgerId = log.logStatusEdit(edit1).ledgerId;
+
+                Map<String, byte[]> customMeta = log
+                    .getBookKeeper()
+                    .openLedgerNoRecovery(ledgerId, BookKeeper.DigestType.MAC, log.getSharedSecret().getBytes(StandardCharsets.UTF_8))
+                    .getCustomMetadata();
+
+                Assert.assertEquals("majordodo", new String(customMeta.get("application"), StandardCharsets.UTF_8));
+                Assert.assertEquals("broker", new String(customMeta.get("component"), StandardCharsets.UTF_8));
+                Assert.assertEquals(BROKER_ID, new String(customMeta.get("broker-id"), StandardCharsets.UTF_8));
             }
 
         }
